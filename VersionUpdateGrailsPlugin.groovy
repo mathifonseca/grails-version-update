40adfc42d38505d62c0454a183466d43d3c606e3
Mathi Fonseca <mathifonseca@gmail.com>
2014-04-30 11:34:08 -0300

    updated plugin version to 1.1.0

--- VersionUpdateGrailsPlugin.groovy
+++ VersionUpdateGrailsPlugin.groovy
@@ -1,10 +1,10 @@
 class VersionUpdateGrailsPlugin {
-    def version = "1.0.0"
+    def version = "1.1.0"
     def grailsVersion = "2.0 > *"
     def title = "Grails Application Version Update Plugin"
     def author = "Mathias Fonseca"
     def authorEmail = "mathifonseca@gmail.com"
-    def description = "Provides a more friendly way to update your application version."
+    def description = "Provides a more friendly way to update your application or plugin version."
     def documentation = "http://grails.org/plugin/version-update"
     def license = "APACHE"
     def issueManagement = [ system: "GITHUB", url: "https://github.com/mathifonseca/grails-version-update/issues" ]
