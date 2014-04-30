99263a4c8be8a380b394b87e4061ac319a06b129
Mathi Fonseca <mathifonseca@gmail.com>
2014-04-30 11:31:22 -0300

    added plugin support

--- scripts/VersionUpdate.groovy
+++ scripts/VersionUpdate.groovy
@@ -140,24 +140,89 @@ private boolean verifyFormat(int depth, String v, String separator, String incre
 
 private String getAppVersion() {
 
-	def applicationPropertiesFile = "${basedir}/application.properties" as File
-	def applicationProperties = new Properties()
-	applicationProperties.load(applicationPropertiesFile.newReader())
-	return applicationProperties['app.version']
-
-}
-
-private boolean setAppVersion(version) {
-
-	boolean result = true
+	def version
 
 	try {
 
-		def applicationPropertiesFile = "${basedir}/application.properties" as File
-		def props = new Properties()
-		props.load(new FileInputStream(applicationPropertiesFile))
-		props['app.version']=version.toString()
-		props.store(new FileOutputStream(applicationPropertiesFile), 'Grails Metadata file')
+		def directory = new File("${basedir}/")
+
+		if (directory?.isDirectory()) {
+
+			def filePattern = ~/^.+GrailsPlugin.groovy$/
+
+			def fileMatches = directory.list( [accept:{d, f-> f ==~ /^.+GrailsPlugin.groovy$/ }] as FilenameFilter)?.toList()
+
+			if (fileMatches?.size() > 0) {
+
+				def pluginDescriptor = "${basedir}/${fileMatches[0]}" as File
+
+				def captured = ''
+
+				pluginDescriptor.getText().find(/def version\s?=\s?[\"|\']?(.*)[\"|\']/) { match, capture -> captured = capture }
+
+				if (captured) version = captured
+
+			}
+
+		}
+
+		if (!version) {
+
+			def applicationPropertiesFile = "${basedir}/application.properties" as File
+			def applicationProperties = new Properties()
+			applicationProperties.load(applicationPropertiesFile.newReader())
+			version = applicationProperties['app.version']
+
+		}
+
+	} catch (Exception ex) {
+		version = null
+	}
+
+	return version
+
+}
+
+private Boolean setAppVersion(version) {
+
+	Boolean result
+
+	try {
+
+		def directory = new File("${basedir}/")
+
+		if (directory?.isDirectory()) {
+
+			def filePattern = ~/^.+GrailsPlugin.groovy$/
+
+			def fileMatches = directory.list( [accept:{d, f-> f ==~ /^.+GrailsPlugin.groovy$/ }] as FilenameFilter)?.toList()
+
+			if (fileMatches?.size() > 0) {
+
+				def pluginDescriptor = "${basedir}/${fileMatches[0]}" as File
+
+				String content = pluginDescriptor.getText()
+
+				content = content.replaceFirst(~/def version\s?=\s?[\"|\']?(.*)[\"|\']/) { "hola" }
+
+				pluginDescriptor.write(content)
+
+				result = true
+
+			}
+
+		}
+
+		if (!result) {
+
+			def applicationPropertiesFile = "${basedir}/application.properties" as File
+			def props = new Properties()
+			props.load(new FileInputStream(applicationPropertiesFile))
+			props['app.version'] = version.toString()
+			props.store(new FileOutputStream(applicationPropertiesFile), 'Grails Metadata file')
+			result = true
+
+		}
 
 	} catch (Exception ex) {
 		result = false
