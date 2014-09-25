220c099f9e86d30f6d12173f26ad8552d4642d98
Mathi Fonseca <mathifonseca@gmail.com>
2014-09-25 15:30:26 -0300

    Version 1.3.0

--- VersionUpdateGrailsPlugin.groovy
+++ VersionUpdateGrailsPlugin.groovy
@@ -1,5 +1,5 @@
 class VersionUpdateGrailsPlugin {
-    def version = '1.2.0'
+    def version = '1.3.0'
     def grailsVersion = "2.0 > *"
     def title = "Grails Application Version Update Plugin"
     def author = "Mathias Fonseca"
