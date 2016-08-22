ef8e43601eaebe9fd20554477b9c217e2d9bdb64
Mathi Fonseca <mathifonseca@gmail.com>
2016-08-22 09:51:35 -0300

    fixed error in description

--- src/main/groovy/VersionUpdateGrailsPlugin.groovy
+++ src/main/groovy/VersionUpdateGrailsPlugin.groovy
@@ -3,7 +3,7 @@ class VersionUpdateGrailsPlugin extends grails.plugins.Plugin {
     def title = "Version Update"
     def author = "Mathias Fonseca"
     def authorEmail = "mathifonseca@gmail.com"
-    def description = "Provides a more friendly way to update your application or plugin version."
+    def description = "Provides a friendlier way to update your application or plugin version."
     def documentation = "http://grails.org/plugin/version-update"
     def license = "APACHE"
     def issueManagement = [ system: "GITHUB", url: "https://github.com/mathifonseca/grails-version-update/issues" ]
